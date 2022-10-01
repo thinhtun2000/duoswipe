612114f3d1480093c9e404c2a6227ba7a882101e
William Le <lequocthinh2305@gmail.com>
2022-10-01 14:42:52 -0600

    Create user info card

---
+++ frontend/src/app/shared/components/user-info-card/user-info-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info-card',
+  templateUrl: './user-info-card.component.html',
+  styleUrls: ['./user-info-card.component.scss']
+})
+export class UserInfoCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
