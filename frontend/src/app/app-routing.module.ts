107d9f0c9acd02368a42c3ea0e31d2a6450c632d
William Le <lequocthinh2305@gmail.com>
2022-10-01 16:30:23 -0600

    Adjust path

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './core/layouts/app-layout/app-layout.component';
 import { HomePageComponent } from './core/pages/home-page/home-page.component';
+import { UserProfileComponent } from './user/pages/user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'app',
     component: AppLayoutComponent,
-    children: [{ path: 'home', component: HomePageComponent }],
+    children: [
+      { path: '', component: HomePageComponent },
+      {
+        path: 'user',
+        loadChildren: () =>
+          import('./user/user.module').then((m) => m.UserModule),
+      },
+    ],
   },
 ];
 
