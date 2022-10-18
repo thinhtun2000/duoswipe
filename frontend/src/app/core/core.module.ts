875aebd77e4952efbbc619eaef9ce2d8b412e2fa
William Le <lequocthinh2305@gmail.com>
2022-10-18 10:35:08 -0600

    Error cards' templates

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -8,6 +8,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CoreRoutingModule } from './core-routing.module';
 import { SimpleAppLayoutComponent } from './layouts/simple-app-layout/simple-app-layout.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UnauthorizedComponent } from './pages/error-pages/unauthorized/unauthorized.component';
+import { ForbiddenComponent } from './pages/error-pages/forbidden/forbidden.component';
+import { NotFoundComponent } from './pages/error-pages/not-found/not-found.component';
+import { ErrorCardComponent } from './components/error-card/error-card/error-card.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +19,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HomePageComponent,
     DashboardComponent,
     SimpleAppLayoutComponent,
+    UnauthorizedComponent,
+    ForbiddenComponent,
+    NotFoundComponent,
+    ErrorCardComponent,
   ],
   imports: [
     CommonModule,
