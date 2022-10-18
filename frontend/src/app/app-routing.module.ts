e1441502072aac754149c74cd1268d826f308b0e
William Le <lequocthinh2305@gmail.com>
2022-10-18 10:56:44 -0600

    Add routes to error pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppLayoutComponent } from './core/layouts/app-layout/app-layout.component';
 import { SimpleAppLayoutComponent } from './core/layouts/simple-app-layout/simple-app-layout.component';
 import { DashboardComponent } from './core/pages/dashboard/dashboard.component';
+import { ForbiddenComponent } from './core/pages/error-pages/forbidden/forbidden.component';
+import { NotFoundComponent } from './core/pages/error-pages/not-found/not-found.component';
+import { UnauthorizedComponent } from './core/pages/error-pages/unauthorized/unauthorized.component';
 import { HomePageComponent } from './core/pages/home-page/home-page.component';
 import { UserProfileComponent } from './user/pages/user-profile/user-profile.component';
 
@@ -26,6 +29,24 @@ const routes: Routes = [
         path: 'user',
         loadChildren: () =>
           import('./user/user.module').then((m) => m.UserModule),
+      },
+    ],
+  },
+  {
+    path: '',
+    component: SimpleAppLayoutComponent,
+    children: [
+      {
+        path: 'not-found',
+        component: NotFoundComponent,
+      },
+      {
+        path: 'forbidden',
+        component: ForbiddenComponent,
+      },
+      {
+        path: 'unauthorized',
+        component: UnauthorizedComponent,
       },
     ],
   },
