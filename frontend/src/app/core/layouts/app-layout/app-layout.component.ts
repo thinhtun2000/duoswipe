0909a1294813f782e79787d8c45e90ca06ad02d8
William Le <lequocthinh2305@gmail.com>
2022-10-18 20:01:08 -0600

    Logout function

--- frontend/src/app/core/layouts/app-layout/app-layout.component.ts
+++ frontend/src/app/core/layouts/app-layout/app-layout.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth/auth.service';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-app-layout',
@@ -7,8 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppLayoutComponent implements OnInit {
   public currentDate: Date = new Date();
+  public user$: Observable<User | null>;
 
-  constructor() {}
+  constructor(
+    private authSvc: AuthService,
+    private router: Router,
+    private userSvc: UserService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.user$ = this.userSvc.user$;
+    this.user$.subscribe((user) => {});
+  }
+
+  logout() {
+    this.authSvc.logout().subscribe((success) => {
+      if (!success) return;
+      this.router.navigateByUrl('/login');
+    });
+  }
 }
