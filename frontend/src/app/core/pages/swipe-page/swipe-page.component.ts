fe2dcf9e1df00a8aeb74439285a662d597ca4e97
William Le <lequocthinh2305@gmail.com>
2022-10-20 19:16:01 -0600

    Mock user for swipe page

--- frontend/src/app/core/pages/swipe-page/swipe-page.component.ts
+++ frontend/src/app/core/pages/swipe-page/swipe-page.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-swipe-page',
   templateUrl: './swipe-page.component.html',
-  styleUrls: ['./swipe-page.component.scss']
+  styleUrls: ['./swipe-page.component.scss'],
 })
 export class SwipePageComponent implements OnInit {
+  public users: Array<User> = [
+    {
+      user_id: '',
+      name: '',
+      password: '',
+      email: '',
+      language_id: 1,
+      location_id: 1,
+      pref_pos: 1,
+      pref_lang: 1,
+      pref_day: '',
+      pref_time: '',
+      pos_1: 1,
+      pos_2: 1,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
