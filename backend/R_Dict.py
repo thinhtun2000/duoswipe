a495e9c3914d1a7be9f8ae3ccf42e09a6878115a
Ruidong Chen <chenruid@nmsu.edu>
2022-10-27 18:38:36 -0600

    Dictionary added

---
+++ backend/R_Dict.py
@@ -0,0 +1,26 @@
+
+rank_id = { 1: "Iron",
+            2: "Bronze",
+            3: "Silver",
+            4: "Gold",
+            5: "Platinum",
+            6: "Diamond",
+            7: "Master",
+            8: "Grandmaster",
+            9: "Challenger"}
+
+
+position_id ={1: "Tank",
+              2: "Bruiser",
+              3: "Jungler",
+              4: "APC",
+              5: "ADC",
+              6: "Support"}
+
+
+location_id = {1: "Las Cruces",
+               2: "El Paso"}
+
+
+language_id = {1: "English",
+               2: "Spanish"}
