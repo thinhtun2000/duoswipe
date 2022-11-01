0c51c5a49187e6d406b64b14992d594ba9dd53b3
William Le <lequocthinh2305@gmail.com>
2022-11-01 11:48:47 -0600

    Wip: matching service

--- frontend/src/app/core/services/matching/matching.service.ts
+++ frontend/src/app/core/services/matching/matching.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { User } from '../../models/user';
+import { UserService } from '../user/user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MatchingService {
+  private MATCHING_API = `${environment.apiBaseURL}matches_update/`;
 
-  constructor() { }
+  public user: User | null;
+
+  constructor(private userSvc: UserService, private http: HttpClient) {
+    this.userSvc.user$.subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+    });
+  }
+
+  public update_match(id_1: string, matchingObject: any) {
+    return this.http.get(this.MATCHING_API + `${id_1}`, matchingObject);
+  }
 }
