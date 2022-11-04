f7abfd61513c23624b8f753a1099a253f88c98c6
Logan Courtright <frenchtoastersrcool@gmail.com>
2022-11-04 00:01:46 -0600

    Design Changes
    Changed the style of some pages, such as the layout designs and the login and register pages

--- backend/app.py
+++ backend/app.py
@@ -3,26 +3,26 @@ from flask_cors import cross_origin
 from flask_login import LoginManager, login_user, logout_user, login_required
 import sys
 
-# # Connect to Mysql
-# DIALECT = 'mysql'
-# DRIVER = 'pymysql'
-# USERNAME = 'root'
-# PASSWORD = 'Crd19991206.'
-# HOST = '127.0.0.1'
-# PORT = '3306'
-# DATABASE = 'duoswipe'
-# URI = '{}+{}://{}:{}@{}:{}/{}?charset=UTF8MB4'.format(
-#     DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
-# )
-#
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = URI
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['CORS_HEADERS'] = 'Content-Type'
-#
-# cors = CORS(app, origins=["http://localhost:4200"])
-#
-# db = SQLAlchemy(app)
+ # Connect to Mysql
+DIALECT = 'mysql'
+DRIVER = 'pymysql'
+USERNAME = 'root'
+PASSWORD = 'Crd19991206.'
+HOST = '127.0.0.1'
+PORT = '3306'
+DATABASE = 'duoswipe'
+URI = '{}+{}://{}:{}@{}:{}/{}?charset=UTF8MB4'.format(
+    DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
+)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+cors = CORS(app, origins=["http://localhost:4200"])
+
+db = SQLAlchemy(app)
 
 
 from connToDB import db, app, cors
@@ -190,6 +190,10 @@ def login():
     # GET
     return render_template('login.html')
 
+
+
+
+
 @app.route('/register', methods=['GET', 'POST'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def register():
@@ -208,6 +212,11 @@ def register():
             return 'fail'
     # GET
     return render_template('login.html')
+
+
+
+
+
 
 
 @app.route('/logout', methods=['GET', 'POST'])
--- frontend/src/assets/js/myjs.js
+++ frontend/src/assets/js/myjs.js
@@ -1,11 +1,11 @@
-/*
+
 function myFunction() {
     document.getElementById("demo").style.color = "red";
 }
 function myFunction2() {
     document.getElementById("demo2").style.color = "blue";
 }
-*/
+
 $('body').removeClass('modal-open');
 $('body').removeAttr('style');
 
