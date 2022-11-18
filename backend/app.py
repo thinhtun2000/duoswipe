8c63a2b0a98c7d71f89c2eddc208a19848745698
Logan Courtright <frenchtoastersrcool@gmail.com>
2022-11-17 22:34:33 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,30 +6,28 @@ from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from connToDB import db, app, cors
 
-#from flask_socketio import SocketIO
+# # Connect to Mysql
+# DIALECT = 'mysql'
+# DRIVER = 'pymysql'
+# USERNAME = 'root'
+# PASSWORD = 'Crd19991206.'
+# HOST = '127.0.0.1'
+# PORT = '3306'
+# DATABASE = 'duoswipe'
+# URI = '{}+{}://{}:{}@{}:{}/{}?charset=UTF8MB4'.format(
+#     DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
+# )
+#
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = URI
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['CORS_HEADERS'] = 'Content-Type'
+#
+# cors = CORS(app, origins=["http://localhost:4200"])
+#
+# db = SQLAlchemy(app)
+# CORS(app)
 
-
-
- # Connect to Mysql
-DIALECT = 'mysql'
-DRIVER = 'pymysql'
-USERNAME = 'root'
-PASSWORD = 'Crd19991206.'
-HOST = '127.0.0.1'
-PORT = '3306'
-DATABASE = 'duoswipe'
-URI = '{}+{}://{}:{}@{}:{}/{}?charset=UTF8MB4'.format(
-    DIALECT, DRIVER, USERNAME, PASSWORD, HOST, PORT, DATABASE
-)
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['CORS_HEADERS'] = 'Content-Type'
-#CORS(app)
-cors = CORS(app, origins=["http://localhost:4200"])
-
-db = SQLAlchemy(app)
 
 
 
