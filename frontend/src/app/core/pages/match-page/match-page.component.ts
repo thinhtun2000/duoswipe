25206944a98c78e25fba2719c39677ab54d20886
William Le <lequocthinh2305@gmail.com>
2022-11-17 21:25:02 -0700

    Match page wip

--- frontend/src/app/core/pages/match-page/match-page.component.ts
+++ frontend/src/app/core/pages/match-page/match-page.component.ts
@@ -1,21 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { MatchApiService } from '../../services/match-api/match-api.service';
+import { UserApiService } from '../../services/user-api/user-api.service';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-match-page',
   templateUrl: './match-page.component.html',
-  styleUrls: ['./match-page.component.scss']
+  styleUrls: ['./match-page.component.scss'],
 })
 export class MatchPageComponent implements OnInit {
+  public user: User | null;
+  public matched: any;
+
+  constructor(
+    private userSvc: UserService,
+    private userApi: UserApiService,
+    private matchApi: MatchApiService
+  ) {}
 
   ngOnInit(): void {
+    this.userSvc.user$.subscribe((user) => {
+      this.user = user;
+      console.log(this.user!.user_id);
+      this.matchApi.getMatched(this.user!.user_id).subscribe((response) => {
+        this.matched = response;
+        console.log(this.matched);
+      });
+    });
   }
-  
-  myScriptElement: HTMLScriptElement;
- 
-  constructor(){
-     this.myScriptElement = document.createElement("script");
-     this.myScriptElement.src = "/assets/js/myjs.js";
-     document.body.appendChild(this.myScriptElement);
-  }
-
 }
